e3a94439360a08b30881f0aadd5880f1cea35313
oneCat <miaoguodong901128@gmail.com>
2017-05-10 23:21:24 +0800

    创建VideoAudioCapture项目文件。

---
+++ VideoAudioCapture/VideoAudioCapture/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  VideoAudioCapture
+//
+//  Created by 苗国栋 on 2017/5/10.
+//  Copyright © 2017年 MiaoGuodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
